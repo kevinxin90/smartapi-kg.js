313168943274cd0c6bdd8a70cc739da268e281c7
jiwen xin <kevinxin@scripps.edu>
2020-07-15 14:38:41 -0700

    add test for filter by api

--- test/index.test.js
+++ test/index.test.js
@@ -18,6 +18,19 @@ describe('test Meta-KG using local specs', () => {
         let res = meta_kg.filter({ predicate: ['treats'] });
         expect(res[0]['association']['predicate']).toBe('treats');
     });
+
+    test("test filter by api", () => {
+        let res = meta_kg.filter({ api: "MyChem.info API" });
+        expect(res[0]['association']['api_name']).toBe("MyChem.info API");
+        res = meta_kg.filter({ api: "Automat PHAROS API" });
+        expect(res[0]['association']['api_name']).toBe("Automat PHAROS API");
+    });
+
+    test("test filter by multiple criteria", () => {
+        let res = meta_kg.filter({ api: "Automat PHAROS API", input_type: "ChemicalSubstance", output_type: "Gene" });
+        expect(res[0]['association']['api_name']).toBe("Automat PHAROS API");
+        expect(res.length).toBe(1);
+    });
 });
 
 describe('test Meta-KG through query SmartAPI API', () => {
