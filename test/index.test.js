cef4aeb69d4553a8682cd919cb0cf4416fa489fc
jiwen xin <kevinxin@scripps.edu>
2020-07-20 09:33:14 -0700

    add test for load biothings only collection

--- test/index.test.js
+++ test/index.test.js
@@ -33,6 +33,44 @@ describe('test Meta-KG using local specs', () => {
     });
 });
 
+describe('test Meta-KG using local specs of BioThings collection only', () => {
+
+    let meta_kg;
+
+    beforeAll(() => {
+        meta_kg = new kg();
+        meta_kg.constructMetaKGSync(category = "biothings");
+    });
+
+    test("test filter", () => {
+        let res = meta_kg.filter({ predicate: 'treats' });
+        expect(res[0]['association']['predicate']).toBe('treats');
+    });
+
+    test("test filter with value as array", () => {
+        let res = meta_kg.filter({ predicate: ['treats'] });
+        expect(res[0]['association']['predicate']).toBe('treats');
+    });
+
+    test("test filter by api", () => {
+        let res = meta_kg.filter({ api: "MyChem.info API" });
+        expect(res[0]['association']['api_name']).toBe("MyChem.info API");
+    });
+
+    test("test filter by multiple criteria", () => {
+        let res = meta_kg.filter({ api: "Automat PHAROS API", input_type: "ChemicalSubstance", output_type: "Gene" });
+        expect(res.length).toBe(0);
+    });
+
+    test("test non-biothings APIs should be excluded", () => {
+        let apis = meta_kg.ops.map(item => item.association.api_name);
+        expect(apis).not.toContain("DGIIdb API");
+        expect(apis).toContain("MyChem.info API");
+    });
+
+
+});
+
 describe('test Meta-KG through query SmartAPI API', () => {
 
     let meta_kg;
