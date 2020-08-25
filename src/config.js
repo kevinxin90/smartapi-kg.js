c57adb87124dd94fcd4299051dcb478f6bc84539
jiwen xin <kevinxin@scripps.edu>
2020-08-25 10:45:52 -0700

    add source field for filter

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    FILTER_FIELDS: ["input_type", "output_type", "predicate", "api_name"],
+    FILTER_FIELDS: ["input_type", "output_type", "predicate", "api_name", "source"],
     SMARTAPI_URL: 'https://smart-api.info/api/query?q=tags.name:translator&size=100'
 }
