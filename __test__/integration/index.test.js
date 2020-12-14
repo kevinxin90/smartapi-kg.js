607beaf3c940a1d4ebec959e279a6d417ea85976
jiwen xin <kevinxin@scripps.edu>
2020-12-14 10:38:01 -0800

    update test

--- __test__/integration/index.test.js
+++ __test__/integration/index.test.js
@@ -9,4 +9,18 @@ describe('Test constructMetaKG from remote', () => {
         expect(meta_kg.ops.length).toBeGreaterThan(0);
         expect(meta_kg.ops[0].association["x-translator"].team).toContain("Text Mining Provider");
     });
-})
+});
+
+describe('Test constructMetaKG from local stored specs', () => {
+    let meta_kg;
+    beforeEach(() => {
+        meta_kg = new kg();
+        meta_kg.constructMetaKGSync();
+    })
+    test("Test construct meta-kg with default translator tag", () => {
+        expect(meta_kg.ops).toBeInstanceOf(Array);
+        expect(meta_kg.ops.length).toBeGreaterThan(0);
+    });
+});
+
+
