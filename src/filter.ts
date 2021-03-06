d5dbcd07480179e360cdc5ec83fc2cb548cc5b9f
jiwen xin <kevinxin@scripps.edu>
2021-03-05 16:30:30 -0800

    refactor: :recycle: refactor filter logics

--- src/filter.ts
+++ src/filter.ts
@@ -1,31 +1,35 @@
 import { FILTER_FIELDS } from './config';
-import _ = require("lodash");
-import { SmartAPIOperation, FilterCriteria, ObjectWithValueAsSet } from './utils/types'
+import _ from 'lodash';
+import { FilterCriteria, ObjectWithValueAsSet } from './types';
+import { SmartAPIKGOperationObject } from './parser/types';
+
+
+const getUniqueValsForEachField = (operations: SmartAPIKGOperationObject[]): ObjectWithValueAsSet => {
+    const allValues = {} as ObjectWithValueAsSet;
+    FILTER_FIELDS.map(field => allValues[field] = new Set())
+    operations.map(operation => {
+        FILTER_FIELDS.map(field => {
+            allValues[field].add(operation.association[field])
+        });
+    });
+    return allValues;
+}
 
 /**
  * filter an array of objects based on the filter criteria
  * @param {Array} ops - an array of objects, each represents an association
  * @param {Object} criteria - the filter criteria
  */
-const filter = (ops: SmartAPIOperation[], criteria: FilterCriteria) => {
-    const allValues = {} as ObjectWithValueAsSet;
+export const ft = (ops: SmartAPIKGOperationObject[], criteria: FilterCriteria) => {
+    const allValues = getUniqueValsForEachField(ops);
     const filters = {} as ObjectWithValueAsSet;
-
-    FILTER_FIELDS.map(field => allValues[field] = new Set())
-    ops.map(item => {
-        FILTER_FIELDS.map(field => {
-            allValues[field].add(item.association[field])
-        });
-    });
 
     FILTER_FIELDS.map(field => {
         if (!(field in criteria) || criteria[field] === undefined) {
             filters[field] = allValues[field]
         } else {
-            if (!Array.isArray(criteria[field])) {
-                criteria[field] = [criteria[field]];
-            };
-            filters[field] = new Set(criteria[field]);
+            const vals = (Array.isArray(criteria[field])) ? criteria[field] : [criteria[field]];
+            filters[field] = new Set(vals);
         }
     });
 
@@ -35,5 +39,3 @@ const filter = (ops: SmartAPIOperation[], criteria: FilterCriteria) => {
     })
     return res;
 }
-
-export = filter;
