c60ae0bced862fb4544d564cf5eb5ba4bc33cfe6
jiwen xin <kevinxin@scripps.edu>
2021-03-05 16:33:04 -0800

    refactor: :recycle: embed smartapi parser

---
+++ src/parser/index.ts
@@ -0,0 +1,120 @@
+import Components from './component';
+import Endpoint from './endpoint';
+import { SmartAPISpec, ParsedAPIMetadataObject, SmartAPIKGOperationObject, APIClass } from './types';
+
+export default class API implements APIClass {
+    private _smartapiDoc: SmartAPISpec;
+    /**
+     * constructor to load SmartAPI specification.
+     * @param {object} smartapiDoc - SmartAPI Specification in Javascript Object format
+     */
+    constructor(smartapiDoc: SmartAPISpec) {
+        this._smartapiDoc = smartapiDoc;
+    }
+
+    get smartapiDoc(): SmartAPISpec {
+        return this._smartapiDoc;
+    }
+
+    get metadata(): ParsedAPIMetadataObject {
+        const metadata = this.fetchAPIMeta();
+        metadata.operations = this.fetchAllOpts();
+        return metadata;
+    }
+    /**
+     * Fetch the title of API from SmartAPI Specification.
+     */
+    private fetchAPITitle(): string | undefined {
+        if (!('info' in this.smartapiDoc)) {
+            return undefined;
+        }
+        return this.smartapiDoc.info.title;
+    }
+
+    private fetchXTranslatorComponent(): string | undefined {
+        if (!('info' in this.smartapiDoc)) {
+            return undefined;
+        }
+        if (!('x-translator' in this.smartapiDoc.info)) {
+            return undefined;
+        }
+        return this.smartapiDoc.info['x-translator'].component;
+    }
+
+    private fetchXTranslatorTeam(): string[] {
+        if (!('info' in this.smartapiDoc)) {
+            return [];
+        }
+        if (!('x-translator' in this.smartapiDoc.info)) {
+            return [];
+        }
+        return this.smartapiDoc.info['x-translator'].team;
+    }
+
+    /**
+     * Fetch the tags associated with the API from SmartAPI Specification.
+     */
+    private fetchAPITags(): string[] {
+        if (!('tags' in this.smartapiDoc)) {
+            return undefined;
+        }
+        return this.smartapiDoc.tags.map((x) => x.name);
+    }
+
+    /**
+     * Fetch the url of the server from SmartAPI Specification.
+     */
+    private fetchServerUrl(): string | undefined {
+        if (!('servers' in this.smartapiDoc)) {
+            return undefined;
+        }
+        return this.smartapiDoc.servers[0].url;
+    }
+
+    /**
+     * Fetch component from SmartAPI Specification.
+     */
+    private fetchComponents() {
+        if (!('components' in this.smartapiDoc)) {
+            return undefined;
+        }
+        return new Components(this.smartapiDoc.components);
+    }
+
+    /**
+     * Fetch metadata information from SmartAPI Specification.
+     */
+    private fetchAPIMeta(): ParsedAPIMetadataObject {
+        return {
+            title: this.fetchAPITitle(),
+            tags: this.fetchAPITags(),
+            url: this.fetchServerUrl(),
+            'x-translator': {
+                component: this.fetchXTranslatorComponent(),
+                team: this.fetchXTranslatorTeam(),
+            },
+            smartapi: {
+                id: this.smartapiDoc._id,
+                meta: this.smartapiDoc._meta,
+            },
+            components: this.fetchComponents(),
+            paths: this.smartapiDoc.paths instanceof Object ? Object.keys(this.smartapiDoc.paths) : [],
+            operations: [],
+        };
+    }
+
+    /**
+     * Fetch all operations from SmartAPI Specification.
+     */
+    private fetchAllOpts(): SmartAPIKGOperationObject[] {
+        let ops = [] as SmartAPIKGOperationObject[];
+        const apiMeta = this.fetchAPIMeta();
+        if ('paths' in this.smartapiDoc) {
+            for (const path of Object.keys(this.smartapiDoc.paths)) {
+                const ep = new Endpoint(this.smartapiDoc.paths[path], apiMeta, path);
+                ops = [...ops, ...ep.constructEndpointInfo()];
+            }
+        }
+        return ops;
+    }
+};
---
+++ src/parser/types.ts
@@ -0,0 +1,191 @@
+export interface XTranslatorObject {
+    component: string;
+    team: string[];
+}
+
+interface SmartAIPInfoObject {
+    title: string;
+    version: string;
+    description?: string;
+    'x-translator'?: XTranslatorObject;
+    [propName: string]: any;
+}
+
+interface SmartAPIServerObject {
+    url: string;
+    description?: string;
+    variables?: any;
+    'x-location'?: string;
+    'x-maturity': any;
+}
+
+export interface SmartAPIReferenceObject {
+    $ref?: string;
+}
+
+export interface SmartAPIParameterObject {
+    name: string;
+    in: 'query' | 'header' | 'path' | 'cookie';
+    description?: string;
+    required?: boolean;
+    deprecated?: boolean;
+    allowEmptyValue?: boolean;
+}
+
+interface SmartAPIMediaTypeObject {
+    example?: any;
+    schema?: any;
+    examples?: any;
+    encoding?: any;
+}
+
+interface SmartAPIRequestBodyObject {
+    description?: string;
+    content: SmartAPIMediaTypeObject[];
+    required?: boolean;
+}
+
+interface SmartAPIResponsesObject {
+    description: string;
+    headers: any;
+    content: any;
+    links: any;
+}
+
+export interface SmartAPIOperationObject {
+    tags?: string[];
+    summary?: string;
+    description?: string;
+    externalDocs?: any;
+    operationID?: string;
+    parameters?: SmartAPIParameterObject[];
+    requestBody?: SmartAPIRequestBodyObject | SmartAPIReferenceObject;
+    responses?: SmartAPIResponsesObject;
+    callbacks?: any;
+    deprecated?: boolean;
+    security?: any;
+    servers?: SmartAPIServerObject[];
+    'x-bte-kgs-operations'?: SmartAPIReferenceObject;
+    [propName: string]: any;
+}
+
+export interface SmartAPIPathItemObject {
+    $ref?: string;
+    summary?: string;
+    description?: string;
+    get?: SmartAPIOperationObject;
+    [propName: string]: any;
+    put?: SmartAPIOperationObject;
+    post?: SmartAPIOperationObject;
+    delete?: SmartAPIOperationObject;
+    options?: SmartAPIOperationObject;
+    head?: SmartAPIOperationObject;
+    patch?: SmartAPIOperationObject;
+    trace?: SmartAPIOperationObject;
+    servers?: SmartAPIServerObject[];
+    parameters?: SmartAPIParameterObject[] | SmartAPIReferenceObject[];
+}
+
+export interface SmartAPIPathsObject {
+    [path: string]: SmartAPIPathItemObject;
+}
+
+interface SmartAPITagObject {
+    name: string;
+    description?: string;
+    externalDocs?: any;
+    'x-id'?: string;
+}
+
+export interface SmartAPIComponentObject {
+    [propName: string]: any;
+}
+
+export interface XBTEKGSOperationBioEntityObject {
+    id: string;
+    semantic: string;
+}
+
+export interface XBTEKGSOperationObject {
+    inputs: XBTEKGSOperationBioEntityObject[];
+    outputs: XBTEKGSOperationBioEntityObject[];
+    predicate: string;
+    source?: string;
+    parameters?: XBTEParametersObject;
+    requestBody?: any;
+    supportBatch?: boolean;
+    inputSeparator?: string;
+    response_mapping?: SmartAPIReferenceObject;
+    responseMapping?: SmartAPIReferenceObject;
+}
+
+export interface SmartAPISpec {
+    openapi: string;
+    info: SmartAIPInfoObject;
+    servers?: SmartAPIServerObject[];
+    paths: SmartAPIPathsObject;
+    tags?: SmartAPITagObject[];
+    security?: any;
+    externalDocs?: any;
+    components?: SmartAPIComponentObject;
+    _id?: string;
+    _meta?: string;
+}
+
+export interface SmartAPIRegistryRecordObject {
+    id: string;
+    meta: any;
+}
+
+interface KGAssociationObject {
+    input_id?: string;
+    input_type: string;
+    output_id?: string;
+    output_type: string;
+    predicate: string;
+    source?: string;
+    api_name?: string;
+    smartapi?: SmartAPIRegistryRecordObject;
+    'x-translator'?: any;
+}
+
+export interface QueryOperationInterface {
+    path: string;
+    method: string;
+    server: string;
+    tags: string[];
+    path_params: string[];
+    params: XBTEParametersObject;
+    request_body: object;
+    supportBatch: boolean;
+    inputSeparator: string;
+}
+
+export interface SmartAPIKGOperationObject {
+    association: KGAssociationObject;
+    query_operation?: QueryOperationInterface;
+    response_mapping?: any;
+    id?: string;
+    tags?: string[];
+}
+
+export interface ParsedAPIMetadataObject {
+    title: string;
+    tags: string[];
+    url: string;
+    paths: string[];
+    components: any;
+    smartapi: SmartAPIRegistryRecordObject;
+    'x-translator': XTranslatorObject;
+    operations: SmartAPIKGOperationObject[];
+}
+
+export interface XBTEParametersObject {
+    [key: string]: string | number;
+}
+
+export interface APIClass {
+    smartapiDoc: SmartAPISpec;
+    metadata: ParsedAPIMetadataObject;
+
+}
