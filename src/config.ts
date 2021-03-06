96a13f6f35be6933a05a6af8934cf6cb60c222c5
jiwen xin <kevinxin@scripps.edu>
2021-03-05 16:30:13 -0800

    refactor: :recycle: update config file

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,14 @@
-import { AppConfig } from './utils/types';
 
-const config: AppConfig = {
-    FILTER_FIELDS: ["input_type", "output_type", "predicate", "api_name", "source"],
-    SMARTAPI_URL: 'https://smart-api.info/api/query?q=tags.name:translator&size=150&fields=paths,servers,tags,components.x-bte*,info,_meta'
-}
+type FIELDS_FOR_FILTER = "input_type" | "output_type" | "predicate" | "api_name" | "source";
 
-export = config;
+export const FILTER_FIELDS: FIELDS_FOR_FILTER[] = ["input_type", "output_type", "predicate", "api_name", "source"];
+
+export const SMARTAPI_URL = 'https://smart-api.info/api/query?q=tags.name:translator&size=150&fields=paths,servers,tags,components.x-bte*,info,_meta'
+
+export const SINGLE_API_SMARTAPI_QUERY_TEMPLATE = 'https://smart-api.info/api/metadata/{smartapi_id}';
+
+export const TEAM_SMARTAPI_QUERY_TEMPLATE = 'https://smart-api.info/api/query?q=info.x-translator.team:"{team_name}"&size=150&fields=paths,servers,tags,components.x-bte*,info,_meta'
+
+export const COMPONENT_SMARTAPI_QUERY_TEMPLATE = 'https://smart-api.info/api/query?q=info.x-translator.component:"{component_name}"&size=150&fields=paths,servers,tags,components.x-bte*,info,_meta'
+
+export const TAG_SMARTAPI_QUERY_TEMPLATE = 'https://smart-api.info/api/query?q=tags.name:{tag_name}&size=150&fields=paths,servers,tags,components.x-bte*,info,_meta'
